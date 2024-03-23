5cabec5667160f36644ee60df6a37cfad59b4d6c
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-03-23 16:43:10 -0400

    .

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,15 +1,6 @@
-import styles from '@/styles/App.module.css';
-// const inter = Inter({ subsets: ['latin'] });
-import React, { Suspense, useRef, useState, useContext, useEffect, use } from 'react';
-import { Canvas, extend, useThree, useFrame } from '@react-three/fiber';
-import Camera from '@/components/Camera';
-import PortalsMain from './portals/PortalsMain';
-import AboutMe from './aboutMe/AboutMe';
-import Floor from './Floor';
+import { useRef, useState, useContext } from 'react';
 import Nav from './Nav';
 import UserContextProvider from '../store/userContext';
-import Mouse from './Mouse';
-import dynamic from 'next/dynamic';
 import Tip from './Tip';
 import Status from './Status';
 import Credits from './Credits';
