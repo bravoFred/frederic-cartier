92267299888702aade2e0607f2a5f50c0fd6bef5
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-04-06 12:55:50 -0400

    .

--- video/MainVideo.tsx
+++ video/MainVideo.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import React from 'react';
+// import Video from 'next-video';
 // import ReactPlayer from 'react-player';
 import ReactPlayer from 'react-player/lazy';
 import styles from './MainVideo.module.scss';
@@ -12,7 +13,8 @@ export default function MainVideo() {
 	const customURL =
 		'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4';
 	// const muxURL = 'https://stream.mux.com/maVbJv2GSYNRgS02kPXOOGdJMWGU1mkA019ZUjYE7VU7';
-	const muxURL = 'https://stream.mux.com/maVbJv2GSYNRgS02kPXOOGdJMWGU1mkA019ZUjYE7VU7k';
+	// const muxURL = 'https://stream.mux.com/maVbJv2GSYNRgS02kPXOOGdJMWGU1mkA019ZUjYE7VU7k';
+	const muxURL = 'https://stream.mux.com/kUjFo8Hy8ltPPFich9n5eob6wI007e028ILU8ie9502RB8.m3u8';
 	// const muxURL = 'https://stream.mux.com/Sc89iWAyNkhJ3P1rQ02nrEdCFTnfT01CZ2KmaEcxXfB008';
 	const DynamicPlayer = dynamic(() => import('react-player/lazy'), {
 		loading: () => <p>loading...</p>,
@@ -20,6 +22,16 @@ export default function MainVideo() {
 	return (
 		<div className={styles.main}>
 			<Header />
+			{/* <MuxPlayer
+				playbackId="EcHgOK9coz5K4rjSwOkoE7Y7O01201YMIC200RI6lNxnhs"
+				// playbackId="https://stream.mux.com/kUjFo8Hy8ltPPFich9n5eob6wI007e028ILU8ie9502RB8.m3u8"
+				accent-color="#000"
+				metadata={{
+					video_id: 'video-id-54321',
+					video_title: 'Test video title',
+					viewer_user_id: 'user-id-007',
+				}}
+			/> */}
 			<p className={styles.title}>Mux</p>
 			<p className={styles.subtitle}>
 				Recommended, seamless, customizable, lots of playback controls, will fit nicely with
@@ -39,14 +51,6 @@ export default function MainVideo() {
 				<DynamicPlayer controls url={ytURL} />
 			</div>
 			<div className={styles.footer}></div>
-			<MuxPlayer
-				playbackId="EcHgOK9coz5K4rjSwOkoE7Y7O01201YMIC200RI6lNxnhs"
-				metadata={{
-					video_id: 'video-id-54321',
-					video_title: 'Test video title',
-					viewer_user_id: 'user-id-007',
-				}}
-			/>
 		</div>
 	);
 }
