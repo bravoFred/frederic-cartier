dbd9e6744ee13526023dc9c69d0d0dad3b4b2255
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-04-06 14:05:34 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,6 +36,7 @@ export default function Header({ title = titleDefault }) {
 			<meta property="og:site_name" content={title} />
 			<meta property="og:description" content={description} />
 
+			<meta name="theme-color" content={'#000000'} />
 			<link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
 			<link rel="apple-touch-icon" sizes="16x16" href="/icons/favicon-16x16.png" />
 			<link rel="apple-touch-icon" sizes="32x32" href="/icons/favicon-32x32.png" />
