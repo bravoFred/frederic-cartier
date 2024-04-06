74e5f1fe500efe8bce691e30f3db3cc9c5bfc596
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-04-06 13:57:12 -0400

    .

--- video/MainVideo.tsx
+++ video/MainVideo.tsx
@@ -6,6 +6,8 @@ import ReactPlayer from 'react-player/lazy';
 import styles from './MainVideo.module.scss';
 import dynamic from 'next/dynamic';
 import MuxPlayer from '@mux/mux-player-react';
+// import MuxPlayer from '@mux/mux-player-react/dist/lazy.mjs';
+
 export default function MainVideo() {
 	// const ytURL = 'https://www.youtube.com/watch?v=XPm7zy-X_fI';
 	const ytURL = 'https://www.youtube.com/watch?v=qEVUtrk8_B4';
@@ -14,9 +16,12 @@ export default function MainVideo() {
 		'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4';
 	// const muxURL = 'https://stream.mux.com/maVbJv2GSYNRgS02kPXOOGdJMWGU1mkA019ZUjYE7VU7';
 	// const muxURL = 'https://stream.mux.com/maVbJv2GSYNRgS02kPXOOGdJMWGU1mkA019ZUjYE7VU7k';
-	const muxURL = 'https://stream.mux.com/kUjFo8Hy8ltPPFich9n5eob6wI007e028ILU8ie9502RB8.m3u8';
-	// const muxURL = 'https://stream.mux.com/Sc89iWAyNkhJ3P1rQ02nrEdCFTnfT01CZ2KmaEcxXfB008';
+	// const muxURL = 'https://stream.mux.com/kUjFo8Hy8ltPPFich9n5eob6wI007e028ILU8ie9502RB8.m3u8';
+	const muxURL = 'https://stream.mux.com/PlOtKFblHHPcnEo014WY8K35eBsp3cFGK44daYCepOPQ.m3u8';
 	const DynamicPlayer = dynamic(() => import('react-player/lazy'), {
+		loading: () => <p>loading...</p>,
+	});
+	const DynamicMuxPlayer = dynamic(() => import('@mux/mux-player-react'), {
 		loading: () => <p>loading...</p>,
 	});
 	return (
@@ -32,25 +37,58 @@ export default function MainVideo() {
 					viewer_user_id: 'user-id-007',
 				}}
 			/> */}
-			<p className={styles.title}>Mux</p>
+			{/* <MuxPlayer
+				streamType="on-demand"
+				playbackId="https://stream.mux.com/PlOtKFblHHPcnEo014WY8K35eBsp3cFGK44daYCepOPQ.m3u8"
+				// playbackId="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4"
+				metadataVideoTitle="Placeholder (optional)"
+				metadataViewerUserId="Placeholder (optional)"
+				primaryColor="#00"
+				secondaryColor="#000000"
+				accent-color="#ffffff"
+				thumbnailTime={80}
+				poster="https://image.mux.com/PlOtKFblHHPcnEo014WY8K35eBsp3cFGK44daYCepOPQ/thumbnail.png?width=1280&height=720&time=80"
+			/> */}
+			{/* use playback id not asset id */}
+			<MuxPlayer
+				className={styles.mux_player}
+				// test video
+				// playbackId="EcHgOK9coz5K4rjSwOkoE7Y7O01201YMIC200RI6lNxnhs"
+				// volta video
+				playbackId="PlOtKFblHHPcnEo014WY8K35eBsp3cFGK44daYCepOPQ"
+				// window video
+				// playbackId="c5GKCMXvMyTwUaTKl6DdsrUMgF28hkYDjn01NOR6Vui00"
+				// metadata={{
+				// 	video_id: 'video-id-54321',
+				// 	video_title: 'Test video title',
+				// 	viewer_user_id: 'user-id-007',
+				// }}
+				thumbnailTime={80}
+				//volta thumbnail
+				poster="https://image.mux.com/PlOtKFblHHPcnEo014WY8K35eBsp3cFGK44daYCepOPQ/thumbnail.png?width=1920&height=1080&time=80"
+				// window thumbnail
+				// poster="https://image.mux.com/c5GKCMXvMyTwUaTKl6DdsrUMgF28hkYDjn01NOR6Vui00/thumbnail.png?width=1280&height=720&time=0"
+				accent-color="#1a1a1a"
+			/>
+			{/* <p className={styles.title}>Mux</p>
 			<p className={styles.subtitle}>
 				Recommended, seamless, customizable, lots of playback controls, fits nicely with
 				your theme
-			</p>
-			<div className={styles.player_wrapper}>
+			</p> */}
+			{/* <div className={styles.player_wrapper}>
 				<DynamicPlayer controls url={muxURL} />
-			</div>
-			<p className={styles.title}>Vimeo</p>
+			</div> */}
+			{/* <p className={styles.title}>Vimeo</p>
 			<p className={styles.subtitle}>For Vimeo functionality down the road if desired</p>
 			<div className={styles.player_wrapper}>
 				<DynamicPlayer controls url={vimeoURL} />
-			</div>
-			<p className={styles.title}>Youtube</p>
+			</div> */}
+			{/* <p className={styles.title}>Youtube</p>
 			<p className={styles.subtitle}>For Youtube functionality down the road if desired</p>
 			<div className={styles.player_wrapper}>
 				<DynamicPlayer controls url={ytURL} />
-			</div>
-			<div className={styles.footer}></div>
+			</div> */}
+			{/* <div className={styles.footer}></div> */}
 		</div>
 	);
 }
