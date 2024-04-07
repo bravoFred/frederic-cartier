f9c2e388d766cd4154cfaa8e038510bb8c56ee10
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-04-07 14:39:06 -0400

    .

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,9 +1,18 @@
 import Image from 'next/image';
 import HeroImg from '@/public/hero.webp';
 import styles from './Content.module.scss';
+import InputContextProvider from '@/store/inputContext';
+import { useContext } from 'react';
 export default function Content() {
+	const { about, films } = useContext(InputContextProvider);
 	return (
-		<div className={styles.content}>
+		<div
+			className={styles.content}
+			style={{
+				filter: about || films ? 'blur(1rem)' : 'none',
+				// height: about || films ? 'calc(100vh - 8rem)' : 'calc(100vh - 8rem)',
+			}}
+		>
 			<Image src={HeroImg} alt="Hero" className={styles.content_hero} />
 		</div>
 	);
--- components/Ticker.tsx
+++ components/Ticker.tsx
@@ -6,6 +6,7 @@ export default function Credits() {
 	const designer = `Frederic Cartier`;
 	const currentYear = new Date().getFullYear();
 	const regular = `${designer} ${currentYear}. All rights reserved. DESIGN`;
+	const { films, about } = useContext(InputContextProvider);
 
 	const developer = `Thomas Matlock`;
 	const dev = `development ${developer}`;
@@ -50,13 +51,19 @@ export default function Credits() {
 		);
 	};
 	return (
-		<div className={styles.credits}>
+		<div
+			className={styles.credits}
+			style={{
+				// transform: films || about ? 'translateY(100%)' : 'translateY(0%)',
+				filter: about || films ? 'blur(1rem)' : 'none',
+			}}
+		>
 			<Child />
 			{/* <Child /> */}
 			{/* <Child /> */}
 			{/* <Child /> */}
 			{/* <Child /> */}
-			<div className={styles.credit_overlay}></div>
+			<div className={styles.credit_overlay} />
 		</div>
 	);
 }
