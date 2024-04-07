af20359594c847334b97e954c99f66de0c1048d1
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-04-07 15:50:32 -0400

    .

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,13 +1,9 @@
 import { useRef, useState, useContext } from 'react';
 import Nav from './Nav';
-import UserContextProvider from '../store/userContext';
-import Status from './Status';
 import Credits from './Ticker';
-import ComingSoon from './ComingSoon';
 import Content from './Content';
 import Modal from './Modal';
 import About from './About';
-import Email from './Email';
 export default function Main() {
 	return (
 		<>
