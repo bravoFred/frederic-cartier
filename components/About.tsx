c60f5d089ec13f476f03ecca3baa008fc2f9f7ff
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-04-07 15:28:50 -0400

    .

--- components/About.tsx
+++ components/About.tsx
@@ -6,6 +6,31 @@ export default function About() {
 	const { aboutMeData } = useContext(DataContext);
 	const { about, contact } = aboutMeData;
 
+	const ContactLinks = () => {
+		return (
+			<ul>
+				{contact.links.map((link) => (
+					<li
+						key={link.title}
+						style={{
+							cursor: 'pointer',
+							listStyleType: 'none',
+							display: 'flex',
+							flexDirection: 'row',
+							width: '100%',
+						}}
+					>
+						<Link href={link.url} target="_blank">
+							<p>
+								{link.title}: {link.urlLabel}
+							</p>
+							{/* <p>{link.title}</p> */}
+						</Link>
+					</li>
+				))}
+			</ul>
+		);
+	};
 	return (
 		<div className={styles.about}>
 			<div className={styles.about_panel}>
@@ -17,6 +42,7 @@ export default function About() {
 			<div className={styles.about_panel}>
 				<div className={styles.about_panel_content}>
 					<h2>{contact.title}</h2>
+					<ContactLinks />
 				</div>
 			</div>
 		</div>
