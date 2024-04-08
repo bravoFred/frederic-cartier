ac5f1795cdb4576032e0c751e66056154d73daa0
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-04-07 20:31:01 -0400

    Update Films2.tsx

--- components/Films2.tsx
+++ components/Films2.tsx
@@ -28,103 +28,215 @@ export default function App() {
 		setSelectedFilm(id);
 		// go to selected film page
 	};
+	// console.log(mobile);
+	const Desktop = () => {
+		return (
+			<Swiper
+				slidesPerView={mobile ? 1.25 : 3.5}
+				mousewheel={true}
+				spaceBetween={mobile ? 16 : 32}
+				grabCursor={true}
+				centeredSlides={true}
+				direction={mobile ? 'vertical' : 'horizontal'}
+				loop={true}
+				pagination={{
+					clickable: true,
+				}}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
+				modules={[Mousewheel, Autoplay, EffectCoverflow]}
+				className={films ? styles.swiper : styles.swiper_hidden}
+				effect={'coverflow'}
+				coverflowEffect={{
+					rotate: 10,
+					stretch: 0,
+					depth: 100,
+					modifier: 1,
+					slideShadows: true,
+				}}
+			>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[0].id)}
+					key={filmsData[0].id}
+				>
+					{filmsData[0].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[1].id)}
+					key={filmsData[1].id}
+				>
+					{filmsData[1].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[2].id)}
+					key={filmsData[2].id}
+				>
+					{filmsData[2].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[3].id)}
+					key={filmsData[3].id}
+				>
+					{filmsData[3].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[4].id)}
+					key={filmsData[4].id}
+				>
+					{filmsData[4].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[0].id)}
+					key={filmsData[0].id}
+				>
+					{filmsData[0].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[1].id)}
+					key={filmsData[1].id}
+				>
+					{filmsData[1].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[2].id)}
+					key={filmsData[2].id}
+				>
+					{filmsData[2].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[3].id)}
+					key={filmsData[3].id}
+				>
+					{filmsData[3].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[4].id)}
+					key={filmsData[4].id}
+				>
+					{filmsData[4].title}
+				</SwiperSlide>
+			</Swiper>
+		);
+	};
+	const Mobile = () => {
+		return (
+			<Swiper
+				slidesPerView={mobile ? 1.25 : 3.5}
+				mousewheel={true}
+				spaceBetween={mobile ? 16 : 32}
+				grabCursor={true}
+				centeredSlides={true}
+				direction={mobile ? 'vertical' : 'horizontal'}
+				loop={true}
+				pagination={{
+					clickable: true,
+				}}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
+				// modules={[Mousewheel, Autoplay, EffectCoverflow]}
+				modules={[Mousewheel, Autoplay]}
+				className={films ? styles.swiper : styles.swiper_hidden}
+				effect={'coverflow'}
+				// coverflowEffect={{
+				// 	rotate: 10,
+				// 	stretch: 0,
+				// 	depth: 100,
+				// 	modifier: 1,
+				// 	slideShadows: mobile ? false : true,
+				// }}
+			>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[0].id)}
+					key={filmsData[0].id}
+				>
+					{filmsData[0].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[1].id)}
+					key={filmsData[1].id}
+				>
+					{filmsData[1].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[2].id)}
+					key={filmsData[2].id}
+				>
+					{filmsData[2].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[3].id)}
+					key={filmsData[3].id}
+				>
+					{filmsData[3].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[4].id)}
+					key={filmsData[4].id}
+				>
+					{filmsData[4].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[0].id)}
+					key={filmsData[0].id}
+				>
+					{filmsData[0].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[1].id)}
+					key={filmsData[1].id}
+				>
+					{filmsData[1].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[2].id)}
+					key={filmsData[2].id}
+				>
+					{filmsData[2].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[3].id)}
+					key={filmsData[3].id}
+				>
+					{filmsData[3].title}
+				</SwiperSlide>
+				<SwiperSlide
+					className={styles.films_list_item}
+					onClick={() => clickHandler(filmsData[4].id)}
+					key={filmsData[4].id}
+				>
+					{filmsData[4].title}
+				</SwiperSlide>
+			</Swiper>
+		);
+	};
 	return (
-		<Swiper
-			slidesPerView={mobile ? 1.25 : 3.5}
-			mousewheel={true}
-			spaceBetween={mobile ? 16 : 32}
-			grabCursor={true}
-			centeredSlides={true}
-			direction={mobile ? 'vertical' : 'horizontal'}
-			loop={true}
-			pagination={{
-				clickable: true,
-			}}
-			autoplay={{
-				delay: 2500,
-				disableOnInteraction: false,
-			}}
-			modules={[Mousewheel, Autoplay, EffectCoverflow]}
-			className={films ? styles.swiper : styles.swiper_hidden}
-			effect={'coverflow'}
-			coverflowEffect={{
-				rotate: 10,
-				stretch: 0,
-				depth: 100,
-				modifier: 1,
-				slideShadows: mobile ? false : true,
-			}}
-		>
-			<SwiperSlide
-				className={styles.films_list_item}
-				onClick={() => clickHandler(filmsData[0].id)}
-				key={filmsData[0].id}
-			>
-				{filmsData[0].title}
-			</SwiperSlide>
-			<SwiperSlide
-				className={styles.films_list_item}
-				onClick={() => clickHandler(filmsData[1].id)}
-				key={filmsData[1].id}
-			>
-				{filmsData[1].title}
-			</SwiperSlide>
-			<SwiperSlide
-				className={styles.films_list_item}
-				onClick={() => clickHandler(filmsData[2].id)}
-				key={filmsData[2].id}
-			>
-				{filmsData[2].title}
-			</SwiperSlide>
-			<SwiperSlide
-				className={styles.films_list_item}
-				onClick={() => clickHandler(filmsData[3].id)}
-				key={filmsData[3].id}
-			>
-				{filmsData[3].title}
-			</SwiperSlide>
-			<SwiperSlide
-				className={styles.films_list_item}
-				onClick={() => clickHandler(filmsData[4].id)}
-				key={filmsData[4].id}
-			>
-				{filmsData[4].title}
-			</SwiperSlide>
-			<SwiperSlide
-				className={styles.films_list_item}
-				onClick={() => clickHandler(filmsData[0].id)}
-				key={filmsData[0].id}
-			>
-				{filmsData[0].title}
-			</SwiperSlide>
-			<SwiperSlide
-				className={styles.films_list_item}
-				onClick={() => clickHandler(filmsData[1].id)}
-				key={filmsData[1].id}
-			>
-				{filmsData[1].title}
-			</SwiperSlide>
-			<SwiperSlide
-				className={styles.films_list_item}
-				onClick={() => clickHandler(filmsData[2].id)}
-				key={filmsData[2].id}
-			>
-				{filmsData[2].title}
-			</SwiperSlide>
-			<SwiperSlide
-				className={styles.films_list_item}
-				onClick={() => clickHandler(filmsData[3].id)}
-				key={filmsData[3].id}
-			>
-				{filmsData[3].title}
-			</SwiperSlide>
-			<SwiperSlide
-				className={styles.films_list_item}
-				onClick={() => clickHandler(filmsData[4].id)}
-				key={filmsData[4].id}
-			>
-				{filmsData[4].title}
-			</SwiperSlide>
-		</Swiper>
+		<>
+			{!mobile && <Desktop />}
+			{mobile && <Mobile />}
+			{/* <Mobile /> */}
+		</>
 	);
 }
