58d473e454a58f1420179c536869e8fffca2945e
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-04-07 21:37:10 -0400

    .

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -3,5 +3,7 @@ import InputContextProvider from '@/store/inputContext';
 import { useContext } from 'react';
 export default function Modal(props: React.PropsWithChildren<{}>) {
 	const { films, about } = useContext(InputContextProvider);
-	return <div className={styles.modal}>{props.children}</div>;
+	return (
+		<div className={films || about ? styles.modal : styles.modal_hidden}>{props.children}</div>
+	);
 }
