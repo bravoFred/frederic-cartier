f340cad5f934386afaebe54c4dffae598209bfee
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2024-04-15 12:12:09 -0400

    Revert "Update Films.tsx"
    This reverts commit 2b0277089de9881e21ff6c111f61d97f6b851ee0.

--- components/Films.tsx
+++ components/Films.tsx
@@ -10,7 +10,6 @@ import 'swiper/css/pagination';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-import 'swiper/css/effect-coverflow';
 import styles from './Films.module.scss';
 // import required modules
 import { useContext } from 'react';
@@ -18,6 +17,7 @@ import InputContextProvider from '@/store/inputContext';
 import DataContextProvider from '@/store/dataContext';
 import UserContextProvider from '@/store/userContext';
 import FilmsVideoPlayer from './FilmsPlayer';
+import 'swiper/css/effect-coverflow';
 
 export default function App() {
 	const { films, showFilm1, toggleFilm1, showFilm2, showFilm3, showFilm4, showFilm5, showFilm6 } =
